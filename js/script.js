9376bab9fb6cea20e52435aadd7129600f2e2eb8
Anna Govedarska <annagovedarska1@gmail.com>
2022-06-23 11:23:13 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-// $(document).ready(function(){
-//    alert("js is loading")
-//   });
+$(document).ready(function(){
+   
+    $('#buttonTownhouses').click(function() {
+        $('.carouselDuplexes').hide();
+        $('.carouselPenthouses').hide();
+        $('.carouselTownhouses').show();
+      });
+
+      $('#buttonDuplexes').click(function() {
+        $('.carouselTownhouses').hide();
+        $('.carouselPenthouses').hide();
+        $('.carouselDuplexes').show();
+      });
+
+      $('#buttonPenthouses').click(function() {
+        $('.carouselTownhouses').hide();
+        $('.carouselDuplexes').hide();
+        $('.carouselPenthouses').show();
+      });
+
+  });
